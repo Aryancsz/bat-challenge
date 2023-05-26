fbe58e449bc780d90185f454e91803d9744a8947
aryanmdh <mallikarjuncrazysoul@gmail.com>
2023-05-26 15:39:38 +0530

    migrate aws-sdk v2 to v3

--- dist/index.js
+++ dist/index.js
@@ -7,24 +7,25 @@ const express_1 = __importDefault(require("express"));
 const dotenv_1 = __importDefault(require("dotenv"));
 const fs_1 = __importDefault(require("fs"));
 const path_1 = __importDefault(require("path"));
-const aws_sdk_1 = __importDefault(require("aws-sdk"));
-const privateKey = fs_1.default.readFileSync(path_1.default.join(__dirname, "./private.pem"));
+const cloudfront_signer_1 = require("@aws-sdk/cloudfront-signer"); // ESM
+const privateKey = fs_1.default.readFileSync(path_1.default.join(__dirname, './private.pem'));
 dotenv_1.default.config();
-const keyPairId = process.env.AWS_KEY_PAIR_ID;
 const app = (0, express_1.default)();
 const port = process.env.PORT;
-const cloudfront = new aws_sdk_1.default.CloudFront.Signer(keyPairId, privateKey.toString());
 const cloudfrontDistributionDomain = "https://d3uxzurwtdsya.cloudfront.net";
 const s3ObjectKey = "aws-logo.png";
 const url = `${cloudfrontDistributionDomain}/${s3ObjectKey}`;
+const keyPairId = process.env.AWS_KEY_PAIR_ID;
 const startDate = new Date();
 startDate.setMinutes(startDate.getMinutes() + 5);
-const expires = startDate.valueOf();
-const response = cloudfront.getSignedUrl({
+const dateLessThan = startDate.toLocaleString(); // any Date constructor compatible
+const signedUrl = (0, cloudfront_signer_1.getSignedUrl)({
     url,
-    expires: expires,
+    keyPairId,
+    dateLessThan,
+    privateKey,
 });
-console.log('<===== signedUrl =====>', response);
+console.log('<===== signedUrl =====>', signedUrl);
 app.get("/", (req, res) => {
     res.send("Express + TypeScript Server");
 });
--- index.ts
+++ index.ts
@@ -1,33 +1,34 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
-import fs from "fs";
-import path from "path";
-import AWS from "aws-sdk";
-const privateKey = fs.readFileSync(path.join(__dirname, "./private.pem"));
-
+import fs from 'fs'
+import path from 'path'
+import { getSignedUrl } from "@aws-sdk/cloudfront-signer"; // ESM
 dotenv.config();
 
+
+const privateKey = fs.readFileSync(path.join(__dirname, './private.pem'))
 const keyPairId = process.env.AWS_KEY_PAIR_ID as string;
+
 
 const app: Express = express();
 const port = process.env.PORT;
 
-const cloudfront = new AWS.CloudFront.Signer(
-  keyPairId,
-  privateKey.toString()
-);
-
 const cloudfrontDistributionDomain = "https://d3uxzurwtdsya.cloudfront.net";
 const s3ObjectKey = "aws-logo.png";
 const url = `${cloudfrontDistributionDomain}/${s3ObjectKey}`;
+// 5min expire  time
 const startDate = new Date();
-startDate.setMinutes(startDate.getMinutes() + 5);
-const expires = startDate.valueOf()
-const response = cloudfront.getSignedUrl({
+startDate.setMinutes(startDate.getMinutes() + 5)
+const dateLessThan = startDate.toLocaleString(); // any Date constructor compatible
+
+const signedUrl = getSignedUrl({
   url,
-  expires: expires,
+  keyPairId,
+  dateLessThan,
+  privateKey,
 });
-console.log('<===== signedUrl =====>', response);
+console.log('<===== signedUrl =====>', signedUrl);
+
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Express + TypeScript Server");
